4ad098f9a2ab377f63c8bedaee076ee5f50f62af
Minh Reigen <mreigen@gmail.com>
2018-03-13 13:52:10 -0700

    comment out chat to show Tim request

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,69 +1,69 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import ChatConnection from './chat_connection_2.js'
-import $ from 'jquery'
+// import React from 'react'
+// import Link from 'gatsby-link'
+// import ChatConnection from './chat_connection_2.js'
+// import $ from 'jquery'
 
-// 'ws://localhost:3001/v1/chat'
+// // 'ws://localhost:3001/v1/chat'
 
-// localhost data
-//http://localhost:8000/chat?myId=parasect&receiver=porygon
-//http://localhost:8000/chat?myId=porygon&receiver=parasect
+// // localhost data
+// //http://localhost:8000/chat?myId=parasect&receiver=porygon
+// //http://localhost:8000/chat?myId=porygon&receiver=parasect
 
-// production data
-//http://localhost:8000/chat?myId=UgKMA2PvpgLUI4dDao0H8pVoJ&receiver=UlFDWkvb780PE9XNUHeQH45mg
-//http://localhost:8000/chat?myId=UlFDWkvb780PE9XNUHeQH45mg
+// // production data
+// //http://localhost:8000/chat?myId=UgKMA2PvpgLUI4dDao0H8pVoJ&receiver=UlFDWkvb780PE9XNUHeQH45mg
+// //http://localhost:8000/chat?myId=UlFDWkvb780PE9XNUHeQH45mg
 
-var query = getQueryParams(document.location.search);
-var myId = query.myId;
-var receiverId = query.receiver;
-console.log('sender id: ' + myId);
-console.log('receiver id: ' + receiverId);
+// var query = getQueryParams(document.location.search);
+// var myId = query.myId;
+// var receiverId = query.receiver;
+// console.log('sender id: ' + myId);
+// console.log('receiver id: ' + receiverId);
 
-// STEP 1: set up a callback function
-function callback(data) {
-  console.log(data);
-  $('#messages').append("<span class='my-name'>" + data.sender.first_name + '</span>: ' + data.content + '<br/>');
-}
+// // STEP 1: set up a callback function
+// function callback(data) {
+//   console.log(data);
+//   $('#messages').append("<span class='my-name'>" + data.sender.first_name + '</span>: ' + data.content + '<br/>');
+// }
 
-// STEP 2: initialize the ChatConnection class
-var chatConn = new ChatConnection('ws://localhost:3001/v1/chat', myId, callback);
+// // STEP 2: initialize the ChatConnection class
+// var chatConn = new ChatConnection('ws://localhost:3001/v1/chat', myId, callback);
 
-function handleKeyPress(event) {
-  if(event.key == 'Enter'){
-    console.log('enter press here! ')
+// function handleKeyPress(event) {
+//   if(event.key == 'Enter'){
+//     console.log('enter press here! ')
 
-    // STEP 3: broadcast the message
+//     // STEP 3: broadcast the message
 
-    var message = event.target.value;
-    chatConn.talk(message, receiverId);
-    event.target.value = '';
+//     var message = event.target.value;
+//     chatConn.talk(message, receiverId);
+//     event.target.value = '';
 
-    $('#messages').append("<span class='my-name'>Me</span>: " + message + '<br/>');
-    return event.preventDefault();
-  }
-}
+//     $('#messages').append("<span class='my-name'>Me</span>: " + message + '<br/>');
+//     return event.preventDefault();
+//   }
+// }
 
-const IndexPage = () => (
-  <div>
-    <h4>Me: {myId}</h4>
-    <h4>Receiver: {receiverId}</h4>
-    <pre id='messages'></pre>
-    <input placeholder='please type something...' autoFocus onKeyPress={handleKeyPress}/>
-  </div>
-)
+// const IndexPage = () => (
+//   <div>
+//     <h4>Me: {myId}</h4>
+//     <h4>Receiver: {receiverId}</h4>
+//     <pre id='messages'></pre>
+//     <input placeholder='please type something...' autoFocus onKeyPress={handleKeyPress}/>
+//   </div>
+// )
 
-function getQueryParams(qs) {
-  qs = qs.split('+').join(' ');
+// function getQueryParams(qs) {
+//   qs = qs.split('+').join(' ');
 
-  var params = {},
-      tokens,
-      re = /[?&]?([^=]+)=([^&]*)/g;
+//   var params = {},
+//       tokens,
+//       re = /[?&]?([^=]+)=([^&]*)/g;
 
-  while (tokens = re.exec(qs)) {
-    params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
-  }
+//   while (tokens = re.exec(qs)) {
+//     params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+//   }
 
-  return params;
-}
+//   return params;
+// }
 
-export default IndexPage
+// export default IndexPage
--- src/pages/chat_connection_2.js
+++ src/pages/chat_connection_2.js
@@ -1,42 +1,102 @@
-import ActionCable from 'actioncable'
+// import ActionCable from 'actioncable'
 
-function ChatConnection(ws_url, senderId, callback) {
-  this.senderId = senderId;
-  this.callback = callback;
+// function ChatConnection(ws_url, senderId, callback) {
+//   this.senderId = senderId;
+//   this.callback = callback;
 
-  this.connection = ActionCable.createConsumer(ws_url);
-  this.roomChannel = this.createRoomConnection();
-}
+//   this.connection = ActionCable.createConsumer(ws_url);
+//   this.notifChannel = this.createNotifConnection();
+//   this.roomChannel = null;
+//   this.receiverId = null;
+// }
 
-ChatConnection.prototype.talk = function(message, receiverId) {
-  this.roomChannel.speak(message, receiverId);
-}
+// ChatConnection.prototype.talk = function(message, receiverId) {
+//   this.notifChannel.speak(message, receiverId);
+// }
 
-ChatConnection.prototype.createRoomConnection = function() {
-  var scope = this;
-  return this.connection.subscriptions.create({
-      channel: 'RoomChannel'
-    }, {
-    connected: function() {
-      console.log('connected to RoomChannel!');
-    },
-    disconnected: function() {},
-    received: function(data) {
-      var theirReceiverId = data.receiver.username;
-      // compares their receiver to self guid (sender guid)
-      // if they match, it means the message is for this user
-      if (theirReceiverId == scope.senderId) {
-        return scope.callback(data);
-      }
-    },
-    speak: function(message, receiverId) {
-      return this.perform('speak', {
-        message:   message,
-        sender:    scope.senderId,
-        receiver:  receiverId
-      });
-    }
-  });
-}
+// ChatConnection.prototype.createNotifConnection = function() {
+//   var scope = this;
+//   return this.connection.subscriptions.create({
+//       channel: 'ChatNotifChannel',
+//       sender: scope.senderId
+//     }, {
+//     connected: function() {
+//       console.log('connected to notification channel!');
+//     },
+//     disconnected: function() {},
+//     // receiver receives a broadcast signal from sender with a roomCode 123
+//     // Receiver checks to see if that's for him
+//     // If it's for him, Receiver then "gets on line 123" to talk to Sender
+//     received: function(data) {
+//       var theirReceiverId = data.receiver.username;
+//       var roomCode        = data.room_code;
 
-export default ChatConnection;
+//       // compares sender to self by guid or username
+//       // if they match, it means the message is for this user
+//       if (theirReceiverId == scope.senderId) {
+//         scope.roomChannel = scope.createRoomConnection(roomCode);
+
+//         // this is so that after user refreshed the page, roomCode is still there. So she won't lose "the line".
+//         // see line 28 if (roomCode)
+//         localStorage.setItem('roomCode', roomCode);
+//       }
+//     },
+//     // broadcast a signal to all listeners on this notif channel
+//     // that a sender (this user) wants to talk to the receiver
+//     // and the room code is 123, telling receiver, please "get on line" 123 to talk to me
+//     speak: function(message, receiverId) {
+//       console.log("trying to get receiver's attention: " + receiverId);
+
+//       scope.receiverId = receiverId;
+
+//       var roomCode = ChatConnection.generateRoomCode();
+//       scope.roomChannel = scope.createRoomConnection(roomCode);
+
+//       localStorage.setItem('firstMessage', message);
+
+//       return this.perform('speak', {
+//         sender:    scope.senderId,
+//         receiver:  receiverId,
+//         room_code: roomCode
+//       });
+//     }
+//   });
+// }
+
+// ChatConnection.prototype.createRoomConnection = function(roomCode) {
+//   var scope = this;
+//   return this.connection.subscriptions.create({channel: 'RoomChannel', room_code: scope.roomCode, sender: scope.senderId, receiver: scope.receiverId}, {
+//     connected: function() {
+//       console.log('connected to RoomChannel. Room code: ' + scope.roomCode);
+//       this.speak('ping');
+//     },
+//     disconnected: function() {},
+//     received: function(data) {
+//       if ((data.content == 'ping') && (data.receiver.username == scope.senderId)) {
+//         this.speak(localStorage.getItem('firstMessage'));
+//       } else {
+//         return scope.callback(data);
+//       }
+//     },
+//     speak: function(message) {
+//       return this.perform('speak', {
+//         room_code: roomCode,
+//         message:   message,
+//         sender:    scope.senderId,
+//         receiver:  scope.receiverId
+//       });
+//     }
+//   });
+// }
+
+// ChatConnection.generateRoomCode = function() {
+//   var length = 8,
+//     charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+//     retVal = "";
+//   for (var i = 0, n = charset.length; i < length; ++i) {
+//     retVal += charset.charAt(Math.floor(Math.random() * n));
+//   }
+//   return retVal;
+// }
+
+// export default ChatConnection;
